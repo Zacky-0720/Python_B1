82bca6e38108b0a7f95a27517850c97892f75d3c
KeitaKoga <koga.keita.lj@mynavi.jp>
2024-05-14 08:18:33 +0000

    キャプションの追加

--- koga/day1/subject.py
+++ koga/day1/subject.py
@@ -1,6 +1,7 @@
 import sys
 args=sys.argv
-#
+
+#点数の入力
 score_math=int(args[1])
 score_Japanese=int(args[2])
 score_English=int(args[3])
@@ -10,7 +11,9 @@ if(((score_math>=70 and score_Japanese>=70 and score_math>=50) or (score_math+sc
 else:
     print("不合格",end="")
 
+#条件1：各科目70点以上または合計点220以上
 if((score_math>=70 and score_Japanese>=70 and score_math>=50) or (score_math+score_Japanese+score_English>=220)):
+   #条件2：全科目が50点以上
    if((score_math>=50)and(score_Japanese>=50)and(score_English>=50)):
     print("合格",end="")
 else:
