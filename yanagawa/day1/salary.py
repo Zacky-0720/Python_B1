e9de60eb9d6d29c51db1e8ca96b877d66fc0811e
RintaroYanagawa <yanagawa.rintaro.xm@mynavi.jp>
2024-05-14 08:24:14 +0000

    fix: be rounded

--- yanagawa/day1/salary.py
+++ yanagawa/day1/salary.py
@@ -8,11 +8,11 @@ threshold = 1000000
 base_salary = min(input_salary, threshold)
 extra_salary = max(0, input_salary - threshold)
 
-salary_tax_amount = base_salary * 0.1
-extra_tax_amount = extra_salary * 0.2
+salary_tax_amount = round(base_salary * 0.1)
+extra_tax_amount = round(extra_salary * 0.2)
 
 tax_amount_sum = salary_tax_amount + extra_tax_amount
 
 pay_amount = input_salary - tax_amount_sum
 
-print(f"支給額:{int(pay_amount)}、税額:{int(tax_amount_sum)}", end="")
+print(f"支給額:{pay_amount}、税額:{tax_amount_sum}", end="")
